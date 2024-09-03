95c7ac124c990adff37426badf3e8909346d3773
firay24 <fisan.onodaconan@gmail.com>
2024-09-03 13:24:32 +0700

    feat: integrated api create message

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -4,7 +4,7 @@ import { Auth, getAuth } from "@/utils/get-auth";
 import { setAuth } from "@/utils/set-auth";
 import { Button, TextInput, Avatar } from "flowbite-react";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import SideBarMenu from "@/components/chat/SidebarMenu";
 import SideBarChat from "@/components/chat/SidebarChat";
@@ -16,6 +16,7 @@ import { useRecoilState } from "recoil";
 
 export default function page() {
   const http = useHttp();
+  const router = useRouter();
   const [chat, setChat] = useState([]);
   const [profile, setProfile] = useRecoilState(profileState);
   const [socket, setSocket] = useState<Socket | null>(null);
@@ -36,6 +37,12 @@ export default function page() {
       userName: "tom",
     },
   ]);
+  const [formChat, setFormChat] = useState({
+    message: "",
+    attachment: "",
+    idConversation: "RLEG1QLR692DMOOQ1L3X",
+    idUser: "",
+  });
 
   const getMessage = async () => {
     try {
@@ -55,7 +62,29 @@ export default function page() {
     }
   };
 
-  const router = useRouter();
+  // const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   const formData = new FormData(event.currentTarget);
+  //   const message = formData.get("message");
+  //   if (message) {
+  //     event.currentTarget.reset();
+  //     socket?.emit("message", { sender: user?.name, message: message as string });
+  //   }
+  // };
+
+  const onSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await http.post<{ name: string; email: string; role: string }>("/v1/message/save", formChat);
+      console.log(response);
+    } catch (error) {}
+  };
+
+  const onLogout = () => {
+    setAuth(null);
+    router.push("/auth");
+  };
+
   useEffect(() => {
     getMessage();
     getProfile();
@@ -75,20 +104,14 @@ export default function page() {
     };
   }, []);
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const message = formData.get("message");
-    if (message) {
-      event.currentTarget.reset();
-      socket?.emit("message", { sender: user?.name, message: message as string });
+  useEffect(() => {
+    if (profile && profile.id) {
+      setFormChat((prevData) => ({
+        ...prevData,
+        idUser: profile.id ?? "",
+      }));
     }
-  };
-
-  const onLogout = () => {
-    setAuth(null);
-    router.push("/auth");
-  };
+  }, [profile]);
 
   return (
     <div className="flex h-full gap-4 p-8">
@@ -117,7 +140,7 @@ export default function page() {
           <form onSubmit={onSubmit} className="mt-auto flex flex-col gap-2">
             <p>{user?.name} </p>
             <div className="flex gap-2">
-              <TextInput name="message" className="w-full" />
+              <TextInput name="message" className="w-full" onChange={(e) => setFormChat((prev) => ({ ...prev, message: e.target.value }))} />
               <Button color="blue" type="submit">
                 Send
               </Button>
