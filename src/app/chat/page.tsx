0ba60f78c515851727f606502c2b43810a6819c1
firay24 <fisan.onodaconan@gmail.com>
2024-09-04 15:05:04 +0700

    feat: integrated realtime chat

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,42 +1,25 @@
 "use client";
 
-import { Auth, getAuth } from "@/utils/get-auth";
-import { setAuth } from "@/utils/set-auth";
 import { Button, TextInput, Avatar } from "flowbite-react";
-import { useRouter } from "next/navigation";
 import { FormEvent, useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import SideBarMenu from "@/components/chat/SidebarMenu";
 import SideBarChat from "@/components/chat/SidebarChat";
 import BubbleChat from "@/components/chat/BubbleChat";
-import { MessageType, Roles } from "@/types/global";
+import { MessageType } from "@/types/global";
 import useHttp from "@/hooks/useHttp";
 import { profileState } from "@/state/profile.state";
 import { useRecoilState } from "recoil";
 
+const SOCKET_SERVER_URL = "http://localhost:4000";
+
 export default function page() {
   const http = useHttp();
-  const router = useRouter();
-  const [chat, setChat] = useState([]);
+  const [chat, setChat] = useState<any[]>([]);
   const [profile, setProfile] = useRecoilState(profileState);
   const [socket, setSocket] = useState<Socket | null>(null);
-  const [messages, setMessages] = useState<{ sender: string; message: string }[]>([]);
-  const [user, setUser] = useState<Auth | null>(null);
-  const [dummyChat, setDummyChat] = useState([
-    {
-      message: "hello",
-      userName: "john",
-    },
-    {
-      message: "hello",
-      userName: "john",
-      type: MessageType.SENDER,
-    },
-    {
-      message: "hello",
-      userName: "tom",
-    },
-  ]);
+  const [receive, setReceive] = useState<any>(null);
+
   const [formChat, setFormChat] = useState({
     message: "",
     attachment: "",
@@ -62,54 +45,69 @@ export default function page() {
     }
   };
 
-  // const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-  //   const formData = new FormData(event.currentTarget);
-  //   const message = formData.get("message");
-  //   if (message) {
-  //     event.currentTarget.reset();
-  //     socket?.emit("message", { sender: user?.name, message: message as string });
-  //   }
-  // };
+  const getMember = async () => {
+    try {
+      const response = await http.get<any>("/v1/member/conversation/RLEG1QLR692DMOOQ1L3X");
+      const receiver = response.data.member.filter((user: any) => user.id !== profile?.id);
+      if (receiver) {
+        setReceive(receive[0]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const onSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    try {
-      const response = await http.post<{ name: string; email: string; role: string }>("/v1/message/save", formChat);
-      console.log(response);
-    } catch (error) {}
-  };
 
-  const onLogout = () => {
-    setAuth(null);
-    router.push("/auth");
+    const payload = {
+      sender: profile?.id,
+      receive: receive?.id,
+      message: formChat.message,
+      attachment: "[]",
+      idConversation: formChat.idConversation,
+    };
+    socket?.emit("message", payload);
+    setFormChat((prev) => ({
+      ...prev,
+      message: "",
+    }));
   };
 
   useEffect(() => {
     getMessage();
     getProfile();
-    if (!localStorage.getItem("user")) router.push("/auth");
-    setUser(localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user") as string) : null);
-
-    const newSocket = io("http://localhost:4000");
-
-    newSocket.on("message", (props: { sender: string; message: string }) => {
-      setMessages((prev) => [...prev, props]);
-    });
-
-    setSocket(newSocket);
-
-    return () => {
-      newSocket.disconnect();
-    };
   }, []);
 
   useEffect(() => {
     if (profile && profile.id) {
+      getMember();
       setFormChat((prevData) => ({
         ...prevData,
         idUser: profile.id ?? "",
       }));
+
+      const newSocket = io(SOCKET_SERVER_URL);
+
+      newSocket.on("connect", () => {
+        newSocket.emit("registerUser", profile.id);
+      });
+
+      newSocket.on("message", (newMessage) => {
+        const newChat = {
+          id: newMessage.Id,
+          message: newMessage.Message,
+          attachment: newMessage.Attachment,
+          member: {
+            email: newMessage.member.email,
+            name: newMessage.member.name,
+            role: newMessage.member.role,
+          },
+        };
+        setChat((prev) => [...prev, newChat]);
+      });
+
+      setSocket(newSocket);
     }
   }, [profile]);
 
@@ -131,22 +129,14 @@ export default function page() {
               </div>
             </Avatar>
           </div>
-          <div className="flex min-h-px w-full grow flex-col gap-4 overflow-auto">
-            {/* {dummyChat.map((item, index) => (
-              <BubbleChat {...item} key={index} />
-            ))} */}
-            {chat && profile && chat.map((item: any, index) => <BubbleChat userName={item.member.name} message={item.message} type={item.member.role === profile.role ? MessageType.SENDER : MessageType.RECEIVER} key={index} />)}
-          </div>
+          <div className="flex min-h-px w-full grow flex-col gap-4 overflow-auto">{chat && profile && chat.map((item: any, index) => <BubbleChat userName={item.member.name} message={item.message} type={item.member.role === profile.role ? MessageType.SENDER : MessageType.RECEIVER} key={index} />)}</div>
           <form onSubmit={onSubmit} className="mt-auto flex flex-col gap-2">
-            <p>{user?.name} </p>
+            <p>{profile && profile.name} </p>
             <div className="flex gap-2">
-              <TextInput name="message" className="w-full" onChange={(e) => setFormChat((prev) => ({ ...prev, message: e.target.value }))} />
+              <TextInput value={formChat.message} name="message" className="w-full" onChange={(e) => setFormChat((prev) => ({ ...prev, message: e.target.value }))} />
               <Button color="blue" type="submit">
                 Send
               </Button>
-              {/* <Button onClick={onLogout} color="failure" type="button">
-              Logout
-            </Button> */}
             </div>
           </form>
         </div>
@@ -154,44 +144,3 @@ export default function page() {
     </div>
   );
 }
-
-// "use client";
-// import { Button, TextInput } from "flowbite-react";
-// import { useEffect, useState } from "react";
-// import io from "socket.io-client";
-
-// const socket = io("/api/socssssket"); // Replace with your server URL
-
-// const Chat = () => {
-//   const [message, setMessage] = useState("");
-//   const [messages, setMessages] = useState<string[]>([]);
-
-//   useEffect(() => {
-//     socket.on("message", (msg) => {
-//       setMessages((prevMessages) => [...prevMessages, msg]);
-//     });
-
-//     return () => {
-//       socket.off("message");
-//     };
-//   }, []);
-
-//   const handleSendMessage = () => {
-//     socket.emit("message", message);
-//     setMessage("");
-//   };
-
-//   return (
-//     <div className="container py-12">
-//       <div>
-//         {messages.map((msg, index) => (
-//           <div key={index}>{msg}</div>
-//         ))}
-//       </div>
-//       <TextInput type="text" value={message} onChange={(e) => setMessage(e.target.value)}></TextInput>
-//       <Button onClick={handleSendMessage}>Send</Button>
-//     </div>
-//   );
-// };
-
-// export default Chat;
